416150ea0283f431c994c6700f78ebcff1365235
jishnu <jishnuthilakp@gmail.com>
2025-12-26 11:27:16 +0530

    models main_event added and registeruser

--- newproj/one/admin.py
+++ newproj/one/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import MainEvents
 
 # Register your models here.
+
+admin.site.register(MainEvents)
--- newproj/one/urls.py
+++ newproj/one/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
 from .views import *
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('home',homepage),
-    path('about',aboutpage)
-]
+    path('about',aboutpage),
+    path('Registration',registration)
+
+    
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
